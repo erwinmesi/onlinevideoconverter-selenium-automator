ee6fa78aa05a1c5de18f3862a2a3ecca45782c9d
Erwin Mesias <mesiaserwin@gmail.com>
2019-04-21 07:28:19 +0800

    Downloads directory config

--- chrome_downloads.py
+++ chrome_downloads.py
@@ -1,10 +1,19 @@
 from os import listdir, path
 import time
+import json
+from pathlib import Path
 
 class ChromeDownloads:
     def has_pending(self):
-        return bool([f for f in listdir('C:\\Users\\mesia\\Downloads') if f.endswith('.crdownload')])
+        downloads_dir = self.get_downloads_dir()
+        filenames = [f for f in listdir(downloads_dir) if f.endswith('.crdownload')]
+        return bool(filenames)
 
     def wait_for_all_to_finish(self):
         while ChromeDownloads().has_pending():
             time.sleep(1)
+    
+    def get_downloads_dir(self):
+        data = Path('config.json').read_text()
+        config = json.loads(data)
+        return config['downloads_dir']
